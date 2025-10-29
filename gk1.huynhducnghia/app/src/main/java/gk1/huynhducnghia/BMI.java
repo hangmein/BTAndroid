7838b8c30d986c43da90c8c385600f019eac03da
hangmein <vietnamesent@gmail.com>
2025-10-29 08:18:19 +0700

    gk1
    gk1

--- gk1.huynhducnghia/app/src/main/java/gk1/huynhducnghia/BMI.java
+++ gk1.huynhducnghia/app/src/main/java/gk1/huynhducnghia/BMI.java
@@ -38,14 +38,42 @@ public class BMI extends AppCompatActivity {
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Calculate();
             }
         });
+    }
+    void Calculate()
+    {
+        String strHeightInput = heightInput.getText().toString().trim();
+        String strWeightInput = weightInput.getText().toString().trim();
+        if (strHeightInput.isEmpty() || strWeightInput.isEmpty())
+        {
+            Toast.makeText(this,"không được bỏ trống số liệu",Toast.LENGTH_SHORT).show();
+            return;
+        }
+            float heightCm = Float.parseFloat(strHeightInput);
+            float weight = Float.parseFloat(strWeightInput);
+            if (heightCm <= 0 || weight <= 0) {
+                Toast.makeText(this, "Chiều cao và cân nặng phải lớn hơn 0", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+            float heightM = heightCm / 100;
+            float bmi = weight / (heightM * heightM);
+
+            bmiValue.setText(String.format("%.1f", bmi));
+            bmiStatus.setText(setBmiStatus(bmi));
+    }
+    public String setBmiStatus(float a)
+    {
+        if (a < 18.5)
+            return "Gầy";
+        else if (a < 24.9)
+            return "Bình thường";
+        else if (a < 29.9)
+            return "Béo";
+        else
+            return "Quá Béo";
     }
 }
