d4f30051b18663ae0018830eafda7d3def7169ac
hangmein <vietnamesent@gmail.com>
2025-11-12 16:12:53 +0700

    fragment

--- Fragment_3/app/src/main/java/ntu/nghia/fragment_3/MainActivity.java
+++ Fragment_3/app/src/main/java/ntu/nghia/fragment_3/MainActivity.java
@@ -1,4 +1,4 @@
-package ntu.nghia.fragment_2;
+package ntu.nghia.fragment_3;
 
 import android.os.Bundle;
 
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .add(R.id.frameContent, new FragmentContent())
-                .add(R.id.frameFooter, new FragmentFooter())
-                .commit();
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
