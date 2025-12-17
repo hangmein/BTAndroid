4d70708fcd5e23676094ff2dc00ae32b37692414
hangmein <vietnamesent@gmail.com>
2025-12-17 08:00:38 +0700

    Update PlayFragment.java

--- project/app/src/main/java/ntu/nghia/project/PlayFragment.java
+++ project/app/src/main/java/ntu/nghia/project/PlayFragment.java
@@ -10,21 +10,10 @@ public class PlayFragment extends Fragment {
 
     public PlayFragment() {
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_play, container, false);
     }
 
-    // Logic của game (ví dụ: load câu hỏi, xử lý đáp án) sẽ được thêm vào onViewCreated sau.
-    /*
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        // Ví dụ: Load câu hỏi đầu tiên
-        // TextView tvQuestion = view.findViewById(R.id.tvQuestionText);
-        // tvQuestion.setText("Đây là câu hỏi đầu tiên...");
-    }
-    */
 }
