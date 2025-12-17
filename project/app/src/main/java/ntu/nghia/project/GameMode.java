b029b8d5422cfede2f57c2fabf0b741afc916e2c
hangmein <vietnamesent@gmail.com>
2025-12-17 07:38:41 +0700

    Update GameMode.java

--- project/app/src/main/java/ntu/nghia/project/GameMode.java
+++ project/app/src/main/java/ntu/nghia/project/GameMode.java
@@ -4,15 +4,13 @@ public class GameMode {
     private int id;
     private String tenCheDo;
     private String moTa;
-    private int anhDaiDien;     
-
+    private int anhDaiDien;
     public GameMode(int id, String tenCheDo, String moTa, int anhDaiDien) {
         this.id = id;
         this.tenCheDo = tenCheDo;
         this.moTa = moTa;
         this.anhDaiDien = anhDaiDien;
     }
-
     public int getId() { return id; }
     public String getTenCheDo() { return tenCheDo; }
     public String getMoTa() { return moTa; }
